4e57372528efcc6cfbe225849c9f10be88d6aaf7
extracold1209 <extracold1209@gmail.com>
2020-02-06 23:39:26 +0900

    torrent fail response

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const bencode = require('bencode');
 const app = new express();
 
 app.use((req, res) => {
     const { info_hash, peer_id, port, uploaded, downloaded, left, numwant, key, compact, supportcrypto, event } = req.params;
 
 
-
-
-    res.send('OK');
+    res.send(bencode.encode({
+        ['failure reason']: 'because I Love You'
+    }));
 });
 
 app.listen(8080, () => {
